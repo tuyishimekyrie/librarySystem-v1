4a9b3915ec841af8d19496acd0531052cb71972f
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-07-26 22:15:27 +0200

    add navbar on the homepage

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/drizzle/db";
 import React from "react";
+import Navbar from "../components/User/Navbar";
 
 const page = async () => {
   const data = await db.query.book.findMany();
@@ -7,9 +8,13 @@ const page = async () => {
 
   return (
     <div>
+      <Navbar />
       <div className="flex space-x-4 m-4">
         {data.map((book) => (
-          <div key={book.id} className="border border-slate-200 p-4 rounded-md bg-white bg-opacity-20">
+          <div
+            key={book.id}
+            className="border border-slate-200 p-4 rounded-md bg-white bg-opacity-20"
+          >
             <h2>{book.title}</h2>
             <p>{book.author}</p>
             <p>{book.isbn}</p>
---
+++ app/components/User/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+      <div className='text-white flex justify-between border-b border-slate-500'>
+          <div>
+          <h1>Logo</h1>
+          <ul></ul>
+          </div>
+          <button>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
