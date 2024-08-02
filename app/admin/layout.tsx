a2e3fe93dd06986e3da7f72aae945fc18e04091a
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-02 20:47:35 +0200

    create admin dashboard

---
+++ app/admin/layout.tsx
@@ -0,0 +1,20 @@
+import Sidebar from "./Sidebar"
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <section>
+   <div className="max-h-screen flex overflow-hidden">
+      <Sidebar />
+      <main className="flex-1  bg-gray-100">
+        {children}
+      </main>
+    </div>
+ 
+      {children}
+    </section>
+  )
+}
---
+++ app/admin/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-black'>  <h1 className="text-2xl font-bold">Dashboard</h1>
+      <p>Welcome to the admin dashboard!</p></div>
+  )
+}
+
+export default page
