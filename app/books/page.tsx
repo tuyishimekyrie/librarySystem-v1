7272718095c1fb74bd62e127266a8d993fb7731c
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-02 19:25:17 +0200

    add image on the book card

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Navbar from "../components/User/Navbar";
 import Link from "next/link";
 import UploadImage from "./UploadImage";
+import Image from "next/image";
 
 const page = async () => {
   const data = await db.query.book.findMany();
@@ -11,18 +12,33 @@ const page = async () => {
   return (
     <div>
       <Navbar />
-      <div className="flex space-x-4 m-4">
+      <div className="flex space-4 m-4 flex-wrap gap-4">
         {data.map((book) => (
           <Link
             href={`/books/${book.id}`}
             key={book.id}
-            className="border border-slate-200 p-4 rounded-md bg-white bg-opacity-20"
+            className="border border-slate-200  rounded-md  bg-opacity-20"
           >
+            {book?.cover ?
+          <Image
+                    src={book?.cover}
+                    alt="Uploaded image"
+                    width={260}  
+                    height={100} 
+                /> : <Image
+                    src={`https://res.cloudinary.com/dvr0mdz82/image/upload/v1712323013/htrxg6wcbwyruo8p5pvx.jpg`}
+                    alt="Uploaded image"
+                    width={260} 
+                    height={100} 
+              />}
+            <div className="p-4">
+
             <h2>{book.title}</h2>
-            <p>{book.author}</p>
-            <p>{book.isbn}</p>
-            <p>{book.description}</p>
+            <p className="text-sm text-gray-300">{book.author}</p>
+            <p className="text-sm text-gray-300">{book.isbn}</p>
+            <p className="text-sm text-gray-300">{book.description}</p>
             {/* <p>{book?.categoryId}</p> */}
+            </div>
           </Link>
         ))}
       </div>
