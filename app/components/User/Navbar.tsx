fb5f4ebcca32a019a9a1be7768335680ded3bf0d
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-02 20:16:10 +0200

    add a link to books page on the navbar

--- app/components/User/Navbar.tsx
+++ app/components/User/Navbar.tsx
@@ -1,12 +1,19 @@
+"use client"
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const Navbar = () => {
+  const router = useRouter();
   return (
       <div className='text-white flex justify-between border-b border-slate-500 px-4 py-2 items-center'>
-          <div>
+          <div className="flex gap-8">
           <Link href="/">Auca Library</Link>
-          <ul></ul>
+        <ul>
+          <span onClick={() => router.push("/books")} className='hover:text-blue-400 hover:cursor-pointer'>Books</span>
+          
+
+          </ul>
           </div>
           <Link href="api/auth/signout" className="bg-red-600 px-4 py-2 rounded-md hover:cursor-pointer hover:bg-red-800">Logout</Link>
     </div>
