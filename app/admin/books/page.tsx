059b90aae9d2b801bc65477cbae2612e7abd40a4
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-03 19:04:31 +0200

    add books table on the admin dashboard

--- app/admin/books/page.tsx
+++ app/admin/books/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { db } from "@/drizzle/db";
+import { book } from "@/drizzle/db/schema";
+import BookTable from "@/app/librarian/books/BookTable";
 
-const page = () => {
+const fetchBooks = async () => {
+  try {
+    const books = await db.select().from(book);
+    return books;
+  } catch (error) {
+    console.error("Error fetching books:", error);
+    return [];
+  }
+};
+
+const BooksPage = async () => {
+  const books = await fetchBooks();
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="p-4 ">
+      <h1 className="text-2xl font-bold mb-4 text-black">Books</h1>
+      <BookTable books={books} />
+    </div>
+  );
+};
 
-export default page
+export default BooksPage;
