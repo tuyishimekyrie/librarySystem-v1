03c68225f3512e6d2fac1c22cbb2b49bf5c05b0d
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-04 10:06:13 +0200

    add logo of auca on the homepage

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -6,6 +6,8 @@ import Button from "./Button";
 import { useRouter } from "next/navigation";
 import { DSpinner } from "react-loadify";
 import "react-loadify/dist/react-loadify.min.css";
+import Image from "next/image";
+import logo from "@/public/aucalogo.png"
 
 const HomePage = () => {
   const { status, data } = useSession();
@@ -15,9 +17,19 @@ const HomePage = () => {
     if (status === "authenticated") router.push("/books", { scroll: false });
   }, [router, status]);
   return (
-    <main className="h-screen flex items-center justify-center">
+    <main className="h-screen flex pt-10  justify-center">
       {status === "loading" ? <span>   <DSpinner size={100} color="blue" borderWidth={5} speed={1000} /></span> :
-        <Button>Sign In</Button>}
+        <div className="flex flex-col gap-40 items-center">
+          <div className="flex flex-col gap-4 items-center">
+
+          <Image src={logo} alt="logo" width={100} height={100} />
+          <span className="mb-14">
+            Auca Library System v1.0
+          </span>
+          </div>
+          <Button >Sign In</Button>
+        </div>
+      }
     </main>
   );
 };
