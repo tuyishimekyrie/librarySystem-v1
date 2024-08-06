17c8c5096bc34f325b9af0a31e7c166a3bb17bc1
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-06 16:51:10 +0200

    resolve the bug that was on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-import { db } from "@/drizzle/db";
 import HomePage from "./components/HomePage";
-import { borrowedbook } from "@/drizzle/db/schema/borrowedbook";
-import { useSession } from "next-auth/react";
-import { DSpinner } from "react-loadify";
 
 export default async function Home() {
-  const { status } = useSession();
-
-  if (status === "loading")
-    return <DSpinner size={50} color="blue" borderWidth={5} speed={1000} />;
-
+  
   return (
     <main className="h-screen flex items-center justify-center">
       <HomePage />
