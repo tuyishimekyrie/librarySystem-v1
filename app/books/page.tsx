fd0a4e78c189b9f8efe2128fefe403193667a5ce
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-08-06 17:54:27 +0200

    remove a bug  that make images not render correctly

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -19,33 +19,34 @@ const page = async () => {
             key={book.id}
             className="border border-slate-200  rounded-md  bg-opacity-20 max-w-80 max-h-96 "
           >
-            {book?.cover ?
-          <Image
-                    src={book?.cover}
-                    alt="Uploaded image"
-                    width={260}  
+            {book?.cover ? (
+              <Image
+                src={book?.cover}
+                alt="Uploaded image"
+                width={260}
                 height={100}
                 className="max-h-44 object-cover"
-                /> : <Image
-                    src={`https://res.cloudinary.com/dvr0mdz82/image/upload/v1712323013/htrxg6wcbwyruo8p5pvx.jpg`}
-                    alt="Uploaded image"
-                    width={260} 
-                height={100} 
-                  className="max-h-44 object-cover"
-              />}
+                />
+              ) : (
+                <Image
+                src={`https://res.cloudinary.com/dvr0mdz82/image/upload/v1712323013/htrxg6wcbwyruo8p5pvx.jpg`}
+                alt="Uploaded image"
+                width={260}
+                height={100}
+                className="max-h-44 object-cover"
+              />
+            )}
             <div className="p-4 py-8">
-
-            <h2>{book.title}</h2>
-            <p className="text-sm text-gray-300">{book.author}</p>
-            {/* <p className="text-sm text-gray-300">{book.isbn}</p> */}
-            {/* <p className="text-sm text-gray-300">{book.description}</p> */}
-            {/* <p>{book?.categoryId}</p> */}
+              <h2>{book.title}</h2>
+              <p className="text-sm text-gray-300">{book.author}</p>
+              {/* <p className="text-sm text-gray-300">{book.isbn}</p> */}
+              {/* <p className="text-sm text-gray-300">{book.description}</p> */}
+              {/* <p>{book?.categoryId}</p> */}
             </div>
           </Link>
         ))}
       </div>
       {/* <UploadImage /> */}
-      
     </div>
   );
 };
